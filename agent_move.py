42fbf686df497d52ea2aa0189b6757931a25d77f
Jaejun Jung <37354854+Jaejuna@users.noreply.github.com>
2022-03-15 14:31:05 +0000

    working on move and rotate algorithm

--- agent_move.py
+++ agent_move.py
@@ -6,11 +6,46 @@ import json
 key = 'b5d5bd555a1501b7324a020b229f7acdbf52afdea9bc2d5f96a74cf6d2e94780'
 ## playerName
 name = 'pia'
+
+##status requests for to get
+status = requests.get('http://20.196.214.79:5050/game/game/view?key={key}&playername={name}')
+json_object = json.loads(status.content) 
+agents = json_object["data"]["message"]["agent_info"]["agent"] 
+
+##view requests for identification
+# 0 = none
+# 1 = agent
+# 2 = explosive
+# 3 = obstacle
+# 4 = projectile
+view = requests.get('http://20.196.214.79:5050/game/view?key={key}&playername={name}') 
+json_object = json.loads(view.content) 
+agent_view = json_object["data"]["message"]["view_info"]["view"] 
+
 ## uid
-id = '111113'
+id = agent_info[name]
 
-status = requests.get('http://20.196.214.79:5050/game/status?key={key}&playername={}') 
-json_object = json.loads(status.content) agents = json_object["data"]["message"]["agent_info"]["agent"] 
+##move direction 
+# 0 = forward
+# 1 = right
+# 2 = backward
+# 3 = left
+type = view_info[type]
+#random direction except forward
+random_directoin = random.randrange(1,3)
 
-# have to make move algorithm
-attack = requests.get('http://20.196.214.79:5050/agent/move?key={key}&uid={id}')
+if type == 0:
+  direction = 0
+elif type == 1:
+  direction = random_directoin
+elif type == 2:
+  direction = 2
+elif type == 3:
+  direction = random_directoin
+elif type == 4:
+  direction = random_directoin
+
+## move api
+move = requests.get('http://20.196.214.79:5050/agent/move?key={key}&uid={id}&direction={direction}')
+
+##### have to identify the enemy properly
--- agent_rotate.py
+++ agent_rotate.py
@@ -5,6 +5,8 @@ import requests
 key = 'b5d5bd555a1501b7324a020b229f7acdbf52afdea9bc2d5f96a74cf6d2e94780'
 ## uid
 id = '111113'
+## angle
+angle = 45
 
-
-attack = requests.get('http://20.196.214.79:5050/agent/attack?key={key}&uid={id}')
+####### have to rotate on right situation
+attack = requests.get('http://20.196.214.79:5050/agent/rotate?key={key}&uid={id}&angle={angle}')
